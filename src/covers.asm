16ee67bf891c7028643a3073d9e52de9e4d8acce
rondnelson99 <rondnelson99@gmail.com>
2021-09-11 14:40:31 -0700

    Added a splash screen and ability to play multiple songs with separate images

---
+++ src/covers.asm
@@ -0,0 +1,26 @@
+
+NUMBER_OF_SONGS equ 2 ;MAKE SURE THIS IS SET PROPERLY
+export NUMBER_OF_SONGS
+
+MACRO CoverImage
+SECTION "Cover Image Data \@", ROM0
+CoverImageData\@:
+INCBIN "res/\1.image"
+
+SECTION "Cover Image Tilemap \@", ROM0
+CoverImageTilemap\@:
+INCBIN "res/\1.imagemap"
+
+SECTION FRAGMENT "Image Table", ROM0
+    dw CoverImageData\@
+    dw SIZEOF("Cover Image Data \@")
+    dw CoverImageTilemap\@
+ENDM
+
+INCLUDE "defines.asm"
+SECTION FRAGMENT "Image Table", ROM0, ALIGN[8] ;for each song, contains a pointer to the cover's tile data, followed by the tilemap.
+
+;Define Cover Images Here, in order of song numbers. As an argument, supply the name of the .png file in 
+ImageTable::
+    CoverImage song1 ;this sets src/res/song1.png as the cover for the first song
+    CoverImage song2
